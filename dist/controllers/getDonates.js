1534803d8abf76370fd82db132d308f0a2ff56e2
fye <keseyf.exe@proton.me>
2025-11-23 17:09:55 -0300

    adjust  getDonates only1 0

--- dist/controllers/getDonates.js
+++ dist/controllers/getDonates.js
@@ -1,5 +1,10 @@
 import prisma from "../utils/prisma.js";
 export default async function getDonates() {
-    return await prisma.donate.findMany();
+    return await prisma.donate.findMany({
+        orderBy: {
+            createdAt: "desc"
+        },
+        take: 10
+    });
 }
 //# sourceMappingURL=getDonates.js.map
